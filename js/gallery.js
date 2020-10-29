5631d4254051d74d255ca47233e443f50bbabef2
Masaaki Furuki <hercode@pm.me>
2020-10-28 18:33:42 -0700

    Add JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,31 @@
 function activateGallery() {
-  alert(document.querySelector("p").textContent);
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo > img");
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newImageAlt = thumbnail.dataset.title;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newImageAlt);
+
+      // Update image info.
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+
+      // Change which image is current class.
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+    });
+  });
 }
